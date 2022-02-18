0b43008b1def84a9ec63c1ae37d63088d10655a3
BarkaouiMohamed <79654063+BarkaouiMohamed@users.noreply.github.com>
2022-02-19 00:37:24 +0100

    Update main.ts

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
+  
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
